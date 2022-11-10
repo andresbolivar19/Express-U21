2a5d0515ad39c9324aabddbb194169667813f5ef
Andres Bolivar <andresbolivar19@gmail.com>
2022-11-09 22:30:11 -0500

    Change port 4000 to 5000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let app = require ('./src/app');
 let connection = require('./src/db/connection');
 
-const PORT = 5000;
+const PORT = process.env.PORT||5000;
 
 app.listen( PORT, ()=> {
     console.log("Server is ready in port", PORT);
